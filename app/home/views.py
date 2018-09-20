e129f0e4e70001f9612252390ac4d7933d63cf81
San <18814184179@163.com>
2018-09-21 07:01:12 +0800

    增加home前台view视图函数

--- app/home/views.py
+++ app/home/views.py
@@ -9,4 +9,54 @@ home = Blueprint('home', __name__, url_prefix='')
 
 @home.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home/index.html')
+
+
+@home.route('/login/')
+def login():
+    return render_template('home/login.html')
+
+
+@home.route('/logout/')
+def logout():
+    return render_template('home/logout.html')
+
+
+@home.route('/register/')
+def register():
+    return render_template('home/register.html')
+
+
+@home.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+
+@home.route('/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+
+@home.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+
+@home.route('/movielog/')
+def moviecol():
+    return render_template('home/moviecol.html')
+
+
+@home.route('/animation/')
+def animation():
+    return render_template('home/animation.html')
+
+
+@home.route('/search/')
+def search():
+    return render_template('home/search.html')
