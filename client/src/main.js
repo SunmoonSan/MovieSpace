304718410d2dbbb888d1009f72a47d16a357f044
San <18814184179@163.com>
2019-09-10 21:12:19 +0800

    增加依赖

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import './assets/base/css/bootstrap.css';
@@ -7,8 +9,10 @@ import './assets/base/css/bootstrap-movie.css';
 import './assets/base/css/animate.css';
 import 'element-ui/lib/theme-chalk/index.css';
 
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Login from './views/login.vue';
-import Register from './views/register.vue';
+import Register from './views/Register.vue';
 
 Vue.use(Router);
 
