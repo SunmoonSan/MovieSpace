4fd7bfe82c908269579846bd2ed33b58183581d7
San <18814184179@163.com>
2019-09-10 21:08:06 +0800

    注册功能

---
+++ app/home/urls.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @desc : Created by San on 2019/9/9 23:53
--- client/src/views/register.vue
+++ client/src/views/register.vue
@@ -4,7 +4,9 @@
       <MHeader />
     </el-header>
     <el-main>
-      <h1>Register</h1>
+      <el-button :plain="true" v-show="false">成功</el-button>
+      <hr />
+      <h1>注册</h1>
 
       <div class="register-form">
         <el-form
@@ -12,18 +14,18 @@
           :rules="rules"
           ref="ruleForm"
           label-width="100px"
-          class="demo-ruleForm"
+          class="ruleForm"
         >
-          <el-form-item label="Email" prop="email">
+          <el-form-item label="邮箱" prop="email">
             <el-input v-model="ruleForm.email"></el-input>
           </el-form-item>
 
-          <el-form-item label="Email" prop="email">
-            <el-input v-model="ruleForm.email"></el-input>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="ruleForm.password" type="password"></el-input>
           </el-form-item>
 
-          <el-form-item label="Email" prop="email">
-            <el-input v-model="ruleForm.email"></el-input>
+          <el-form-item label="确认密码" prop="confirmPwd">
+            <el-input v-model="ruleForm.confirmPwd" type="password"></el-input>
           </el-form-item>
 
           <el-form-item>
@@ -34,7 +36,7 @@
       </div>
     </el-main>
 
-    <el-footer>
+    <el-footer style="height: 100%;">
       <m-footer />
     </el-footer>
   </el-container>
@@ -51,16 +53,76 @@ export default {
   data() {
     return {
       ruleForm: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        email: ""
+      },
+      rules: {
+        email: [
+          {
+            required: true,
+            type: "email",
+            message: "请输入邮箱",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            type: "string",
+            message: "请输入密码",
+            trigger: "blur"
+          }
+        ],
+        confirmPwd: [
+          {
+            required: true,
+            type: "string",
+            message: "请再次输入密码",
+            trigger: "blur"
+          },
+          {
+            validator: (rule, value, callback) => {
+              if (value === "") {
+                callback(new Error("请再次输入密码"));
+              } else if (value !== this.ruleForm.password) {
+                callback(new Error("两次密码输入不一致"));
+              } else {
+                callback();
+              }
+            },
+            trigger: "blur"
+          }
+        ]
       }
     };
+  },
+  methods: {
+    submitForm(formName) {
+      let self = this;
+      console.log("this");
+      console.log(this);
+      this.$refs[formName].validate(valid => {
+        console.log(valid);
+        console.log(this.rules);
+        if (valid) {
+          this.axios
+            .post("http://127.0.0.1:5000/register", {
+              email: this.ruleForm.email,
+              password: this.ruleForm.password
+            })
+            .then(function(res) {
+              if (res.data.code == 0) {
+                self.$message.success("注册成功!");
+              } else {
+                self.$message.error("注册失败, " + res.data.msg);
+                console.error(res.data);
+              }
+            })
+            .catch(function(error) {
+              console.error(error);
+            });
+        }
+      });
+    }
   }
 };
 </script>
@@ -70,18 +132,24 @@ export default {
 .el-footer {
   background-color: #b3c0d1;
   color: #333;
+  padding: 0px;
   text-align: center;
-  line-height: 60px;
+  height: 80px;
+  line-height: 80px;
 }
 
 .el-main {
   background-color: #e9eef3;
   color: #333;
-  text-align: center;
-  line-height: 160px;
+  height: 600px;
 }
 
 body > .el-container {
-  margin-bottom: 40px;
+  margin-bottom: 10px;
+}
+
+.register-form {
+  width: 400px;
+  margin: auto;
 }
 </style>
