942eb4634d4c95a901eb4a513f8605236423c42a
San <18814184179@163.com>
2019-09-10 21:15:21 +0800

    util工具包

--- app/utils/response.py
+++ app/utils/response.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @desc : Created by San on 2019/9/10 18:53
+from flask import jsonify
+
+
+def make_response(code=0, data=None, msg='Success'):
+    """
+    构造返回的JSON数据
+    :param code: 状态码
+    :param data: 数据
+    :param msg: 返回描述信息
+    :return:
+    """
+    resp_data = {'code': code, 'msg': msg}
+    if data is not None:
+        resp_data.update({'data': data})
+    return jsonify(resp_data)
