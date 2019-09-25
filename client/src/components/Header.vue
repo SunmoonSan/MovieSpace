b2e3921379375a43b2e53897f7900401373c2df7
San <18814184179@163.com>
2019-09-25 18:57:43 +0800

    Header.vue

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,69 +1,72 @@
 <template>
-  <div class="m-header">
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <!--小屏幕导航按钮和logo-->
-        <div class="navbar-header">
-          <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a href="index.html" class="navbar-brand" style="height:40px;width:250px;">
-            <!-- <img src="@/assets/base/images/logo.png" style="height:40px;weight:40px;" />微电影 -->
-          </a>
-        </div>
-        <!--小屏幕导航按钮和logo-->
-        <!--导航-->
-        <div class="navbar-collapse collapse">
-          <form class="navbar-form navbar-left" role="search" style="margin-top:18px;">
-            <div class="form-group input-group">
-              <input type="text" class="form-control" placeholder="请输入电影名！" />
-              <span class="input-group-btn">
-                <a class="btn btn-default" href="search.html">
-                  <span class="glyphicon glyphicon-search"></span>&nbsp;搜索
-                </a>
-              </span>
-            </div>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a class="curlink" href="/">
-                <span class="glyphicon glyphicon-film"></span>&nbsp;电影
-              </a>
-            </li>
-            <li>
-              <a class="curlink" href="/login">
-                <span class="glyphicon glyphicon-log-in"></span>&nbsp;登录
-              </a>
-            </li>
-            <li>
-              <a class="curlink" href="/register">
-                <span class="glyphicon glyphicon-plus"></span>&nbsp;注册
-              </a>
-            </li>
-            <li>
-              <a class="curlink" href="/logout">
-                <span class="glyphicon glyphicon-log-out"></span>&nbsp;退出
-              </a>
-            </li>
-            <li>
-              <a class="curlink" href="/user">
-                <span class="glyphicon glyphicon-user"></span>&nbsp;会员
-              </a>
-            </li>
-          </ul>
-        </div>
-        <!--导航-->
-      </div>
-    </nav>
-  </div>
+  <el-header style="padding:0 0">
+    <b-navbar toggleable="lg" type="dark" variant="info" class="header">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <!-- <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>-->
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#" @click="handleLogout">退出</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </el-header>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  methods: {
+    // 退出
+    handleLogout() {
+      this.$axios
+        .get("logout")
+        .then(res => {
+          if (res.status == 200 && res.data.code == 0) {
+            this.$message({
+              message: "已退出",
+              type: "success"
+            });
+            this.$router.push("/");
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  height: 60px;
+  width: 100%;
+}
+</style>
