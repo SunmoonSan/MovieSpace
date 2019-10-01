34d6db7c3819ac22962951ab4382a48008199355
San <18814184179@163.com>
2019-10-01 09:10:50 +0800

    收藏电影

--- app/home/movie_views.py
+++ app/home/movie_views.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @desc : Created by San on 2019/9/26 23:27
+from flask import request
 from flask_restful import Resource
 
-from app.models import Movie
+from app import db
+from app.auth.auth_views import Auth
+from app.models import Movie, Moviecol
 from app.utils.response import make_response
 
 
@@ -16,10 +19,30 @@ class MovieView(Resource):
             return make_response(
                 code=0, data={
                     'id': movie.id,
-                    'title':movie.title,
+                    'title': movie.title,
                     'imageLink': movie.logo,
                     'videoLink': movie.url,
+                    'length': movie.length,
+                    'area': movie.area,
+                    'releaseTime': movie.release_time,
                     'info': movie.info,
                 },
                 msg='Success')
         return make_response(code=1, msg='该电影不存在!')
+
+
+class MovieCollectView(Resource):
+
+    def post(self, movie_id):
+        resp = Auth.identify(request.headers)
+        if not resp['allow_access']:
+            return make_response(code=1, msg=resp['msg'])
+
+        user_id = resp['user_id']
+
+        try:
+            db.session.add(Moviecol(movie_id=movie_id, user_id=user_id))
+            db.session.commit()
+            return make_response(code=0, msg='Success')
+        except Exception as  err:
+            return make_response(code=1, msg=str(err))
