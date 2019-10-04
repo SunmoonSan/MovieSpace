aa86498b8ea1666b97018489be1d580cd36274a3
San <18814184179@163.com>
2019-10-04 23:24:41 +0800

    树形菜单

--- client/src/components/TreeMenu.vue
+++ client/src/components/TreeMenu.vue
@@ -1,54 +1,48 @@
 <template>
-  <el-row class="tac">
-    <el-col :span="6">
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-      >
-        <router-link to="/home">
-          <el-menu-item index="1">
-            <i class="el-icon-menu"></i>
-            <span slot="title">首页</span>
-          </el-menu-item>
-        </router-link>
-        <router-link to="/tag/list">
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">标签管理</span>
-          </el-menu-item>
-        </router-link>
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>标签管理</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-  </el-row>
+  <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+    <router-link :to="{name:'admin'}">
+      <el-menu-item index="1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+    </router-link>
+    <router-link :to="{name:'movie-list'}">
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">电影管理</span>
+      </el-menu-item>
+    </router-link>
+    <router-link :to="{name:'tag-list'}">
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">标签管理</span>
+      </el-menu-item>
+    </router-link>
+    <router-link :to="{name:'preview-list'}">
+      <el-menu-item index="3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">预告管理</span>
+      </el-menu-item>
+    </router-link>
+    <router-link :to="{name:'auth-list'}">
+      <el-menu-item index="3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">权限管理</span>
+      </el-menu-item>
+    </router-link>
+    <router-link :to="{name:'role-list'}">
+      <el-menu-item index="3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">角色管理</span>
+      </el-menu-item>
+    </router-link>
+    <router-link :to="{name:'admin-list'}">
+      <el-menu-item index="3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">管理员管理</span>
+      </el-menu-item>
+    </router-link>
+  </el-menu>
 </template>
 
 <script>
